455da1f668828fb16d1f20f8c9ab63987f191d4b
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-20 19:17:42 -0600

    fix didn't work

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 //importing environmental variables
 require('dotenv').config();
 const mongoose = require('mongoose');
-const mongoDBURI = 'mongodb://localhost:27017/' + 'gardens'
+const mongoDBURI = 'mongodb://localhost:27017/' + 'gardenapi'
 const config = { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify:false }
 mongoose.connect(mongoDBURI, config);
 
